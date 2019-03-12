4f6854a54723a7975b50633de6f2c7866ff00e4b
Albano Pena <c-albano.pena@charter.com>
2019-03-12 16:00:26 -0600

    Added classes, pacakeges and more examples

---
+++ Java Fundamentals/src/fundamental/programming/structures/ImportsAndPackages.java
@@ -0,0 +1,24 @@
+package fundamental.programming.structures;
+
+import java.util.Random; //immport busca en el packages java.util la clase Random
+//import java.util.*; // imports java.util.Random y otras clases
+
+//import java.lang.System;
+//import java.lang.*;
+//import java.util.Random;
+//import java.util.*;
+
+//import java.util.*;
+import java.sql.*; 
+import java.util.Date;
+
+public class ImportsAndPackages {
+
+	public static void main(String[] args) {
+		Date date;
+		Random r = new Random(); 
+		System.out.println(r.nextInt(10));//Se imprime numero random entre 0 y 9
+
+	}
+
+}
